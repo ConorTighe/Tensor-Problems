1eeaa75ab71bba90849ebbd24ceb3e42d74db476
Conor <conor.tighe22@gmail.com>
2017-11-14 17:37:05 +0000

    Training done

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-"# Tensor-Problems" 
+# TensorFlow Problem sheet
+The following is the solutions to the [problem sheet](https://github.com/emerging-technologies/emerging-technologies.github.io/blob/master/problems/tensorflow.md) that is part of the emerging technologies module.
+
+## Set up:
+To run the application you must set up tensorflow which can be done using anaconda, the offical tensorflow tutorial can be found [here](https://www.tensorflow.org/install/). You must also install [Python 3.6](https://anaconda.org/anaconda/python). The Iris dataset is also required for this app and is included in the repository.
+
+
+
--- tensor.py
+++ tensor.py
@@ -1,36 +1,43 @@
-import tensorflow as tf
+import os
 import numpy as np
+import tensorflow as tf
 
-# Csv Name
-IRIS_TRAINING = "IRIS.csv"
+# I use this line to suppress warnings, tensorflow tells me that my machine can run tensorflow faster with some configuration.
+# This line is not necessary but makes the output cleaner
+os.environ['TF_CPP_MIN_LOG_LEVEL']='2'
 
-# Load datasets.
-print("Loading dataset...")
+# IRIS data set
+IRIS = "IRIS.csv"
 
-Iris_set = tf.contrib.learn.datasets.base.load_csv_with_header(
-filename=IRIS_TRAINING,
-target_dtype=np.int,
-features_dtype=np.float32)
+def main():
+  # Load data from csv.
+  print("Loading IRIS..")
+  training_set = tf.contrib.learn.datasets.base.load_csv_with_header(
+      filename=IRIS,
+      target_dtype=np.int,
+      features_dtype=np.float32)
 
-# Specify that all features have real-value data
-feature_columns = [tf.feature_column.numeric_column("x", shape=[4])]
+  # Prepare data for modeling by checking the columns with tf.feature_column.numeric_column.
+  # We have the Petal length + width and the Speal length + width so we set the shape to 4 since there are 4 columns to feature
+  print("Checking values..")
+  feature_columns = [tf.feature_column.numeric_column("x", shape=[4])]
 
-# Build 3 layer DNN with 10, 20, 10 units respectively.
-classifier = tf.estimator.DNNClassifier(feature_columns=feature_columns,
+  # Fit our data to a Deep Neural Network Classifier Model and make a temp directory to hold it
+  classifier = tf.estimator.DNNClassifier(feature_columns=feature_columns,
                                           hidden_units=[10, 20, 10],
                                           n_classes=3,
-                                          model_dir="/tmp/iris_model")
-# Define the training inputs
-train_input_fn = tf.estimator.inputs.numpy_input_fn(
-    x={"x": np.array(Iris_set.data)},
-    y=np.array(Iris_set.target),
-    num_epochs=None,
-    shuffle=True)
+                                          model_dir="Model")
+  # Define the training inputs
+  print("Preparing training with inputs..")
+  train_input_fn = tf.estimator.inputs.numpy_input_fn(
+      x={"x": np.array(training_set.data)},
+      y=np.array(training_set.target),
+      num_epochs=None,
+      shuffle=True)
 
-# Train model.
-classifier.train(input_fn=train_input_fn, steps=2000)
+  # Train model.
+  print("Training model..")
+  classifier.train(input_fn=train_input_fn, steps=2000)
 
-# Evaluate accuracy.
-accuracy_score = classifier.evaluate(input_fn=train_input_fn)["accuracy"]
-
-print("\nTest Accuracy: {0:f}\n".format(accuracy_score))
+if __name__ == "__main__":
+    main()
