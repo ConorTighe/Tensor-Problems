3c093643ee40580d5c0c7c4f19d6e1a4e9eacb9b
Conor <conor.tighe22@gmail.com>
2017-11-21 10:58:36 +0000

    final work done

--- tensor.py
+++ tensor.py
@@ -14,13 +14,13 @@ def main():
   # Load data from csv.
   print("Loading IRIS..")
   fulldata = pd.read_csv(IRIS, header=None)
-
+  
   # Prepare data for modeling by checking the columns with tf.feature_column.numeric_column.
   # We have the Petal length + width and the Speal length + width so we set the shape to 4 since there are 4 columns to feature
-  print("Checking values..")
   feature_columns = [tf.feature_column.numeric_column("x", shape=[4])]
 
   # Fit our data to a Deep Neural Network Classifier Model and make a temp directory to hold it
+  print("Creating Model..")
   classifier = tf.estimator.DNNClassifier(feature_columns=feature_columns,
                                           hidden_units=[10, 20, 10],
                                           n_classes=3,
@@ -28,22 +28,22 @@ def main():
   
   # Display the full dataset to the user and
   # Split our mixed up IRIS data into training and testing
-  print("Full data:")
+  print("IRIS Dataset:")
   print(fulldata)
-  training_set = pd.read_csv(IRIS, header=None, nrows=100)
-  test_set = pd.read_csv(IRIS, header=None, skiprows=100, nrows=151, names = ["100", "4", "setosa", "versicolor", "virginica"])
+  training_set = pd.read_csv(IRIS, header=None, nrows=100 , names = ["100", "4", "setosa", "versicolor", "virginica"])
+  test_set = pd.read_csv(IRIS, header=None, skiprows=100, nrows=151, names = ["51", "4", "setosa", "versicolor", "virginica"])
 
   # Set the file name for both CSV
   train_data = "trainIRIS.csv"
   test_data = "testIRIS.csv"
 
   # Save them to individual CSV files
-  training_set.to_csv(train_data, index = False, header=None)
+  training_set.to_csv(train_data, index = False, header=True)
   test_set.to_csv(test_data, index = False, header=True)
 
   # Create training dataset
   training_set = tf.contrib.learn.datasets.base.load_csv_with_header(
-      filename=IRIS,
+      filename=train_data,
       target_dtype=np.int,
       features_dtype=np.float32)
 
@@ -79,6 +79,7 @@ def main():
   print("\nTest Accuracy: {0:f}\n".format(accuracy_score))
 
   # Predict the test array
+  print("Predicting data..")
   predict_input_fn = tf.estimator.inputs.numpy_input_fn(x={"x": np.array(test.data)},
   y=np.array(test.target),
   num_epochs=1,
@@ -89,9 +90,8 @@ def main():
   predicted_classes = [p["classes"] for p in predictions]
 
   # Display predicted classes
-  print(
-    "Test Set Class Predictions:    {}\n"
-    .format(predicted_classes))
+  print("Prediction results:")
+  print('[%s]' % ', '.join(map(str, predicted_classes)))
 
 
 if __name__ == "__main__":
